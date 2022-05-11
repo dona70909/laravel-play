46ccb4b78fb3f3675e3faee6dd5ac628e2e059b8
Donatella7 <campaniellodonatella@gmail.com>
2022-05-11 23:07:01 +0200

    comment web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
+
 use App\Http\Controllers\PetShop;
-use App\Http\Controllers\classes\Product;
 use Illuminate\Support\Facades\Route;
+//use App\Http\Controllers\classes\Product;
+//use Illuminate\Support\Facades\Auth;
 
 
 
@@ -17,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/* esegue il metodo createProducts inside the Classe PetShop e 
+questo metodo oltre a "ritornate" la view porta con se anche i dati  */
 Route::get('/',[PetShop::class, 'createProducts'],function () {
 })->name("home");
 
@@ -27,9 +29,9 @@ Route::get('/login', function () {
     return view('guest.login');
 })->name("login");
 
-
+/* esegue il metodo createOrder inside the Classe PetShop e 
+questo metodo oltre a "ritornate" la view porta con se anche i dati  */
 Route::get('/basket',[PetShop::class, 'createOrder'], function () {
-    //return view('guest.basket');
 })->name("basket");
 
 

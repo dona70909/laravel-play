95b38863b325181619df6f1ba786da14b9fc276f
Donatella7 <campaniellodonatella@gmail.com>
2022-05-11 19:42:14 +0200

    style card products

--- resources/views/partials/main/main_products.blade.php
+++ resources/views/partials/main/main_products.blade.php
@@ -1,12 +1,14 @@
 
 <section class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            ciao sono il main product
-
-            @php
-                var_dump($products)
-            @endphp
-        </div>
+    <div class="row p-4 flex-wrap my-product-card-container">
+        @foreach ($products as $product) 
+            <div class="card col-3 my-product-card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{$product->getNameProduct()}}</h5>
+                    <p class="card-text">{{$product->getNameProduct()}}</p>
+                </div>
+            </div>
+        @endforeach
     </div>
 </section>
